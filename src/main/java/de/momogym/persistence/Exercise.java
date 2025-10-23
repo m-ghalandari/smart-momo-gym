e67a58169e834ea8d0e85f9e45fdfbc0923713a9
Mamali99 <mohammadghalandari1998@gmail.com>
2025-10-23 15:07:30 +0200

    initial project

---
+++ src/main/java/de/momogym/persistence/Exercise.java
@@ -0,0 +1,81 @@
+package de.momogym.persistence;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "EXERCISE_CATALOG")
+public class Exercise {
+
+    @Id
+    @SequenceGenerator(
+            name = "exercise_seq_gen",
+            sequenceName = "EXERCISE_SEQ",
+            initialValue = 1,
+            allocationSize = 50
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "exercise_seq_gen"
+    )
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "TARGET_MUSCLE")
+    private String targetMuscle;
+
+    @Column(name = "GIF_URL")
+    private String gifUrl;
+
+    @Column(name = "VIDEO_LINK")
+    private String vidoeLink;
+
+    public Exercise() {
+    }
+
+    public Exercise(String name, String targetMuscle, String gifUrl, String vidoeLink){
+        this.name = name;
+        this.targetMuscle = targetMuscle;
+        this.gifUrl = gifUrl;
+        this.vidoeLink = vidoeLink;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTargetMuscle() {
+        return targetMuscle;
+    }
+
+    public void setTargetMuscle(String targetMuscle) {
+        this.targetMuscle = targetMuscle;
+    }
+
+    public String getGifUrl() {
+        return gifUrl;
+    }
+
+    public void setGifUrl(String gifUrl) {
+        this.gifUrl = gifUrl;
+    }
+
+    public String getVidoeLink() {
+        return vidoeLink;
+    }
+
+    public void setVidoeLink(String vidoeLink) {
+        this.vidoeLink = vidoeLink;
+    }
+}
