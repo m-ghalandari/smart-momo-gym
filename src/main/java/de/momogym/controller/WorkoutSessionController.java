28e6ad365cabb402842c6fc5900b46c784371b0c
Momo <mohammadghalandari1998@gmail.com>
2026-03-07 18:45:26 +0100

    Feature: Dynamisches Verwalten von geplanten Übungen im WorkoutSession-Workflow hinzugefügt.
    Die Pending-Exercises-Liste wurde eingeführt, um erledigte Übungen dynamisch zu verwalten. Neue Aktionen `markAsDone` und `finishWorkout` hinzugefügt. Ajax-Integration in `workoutSession.xhtml`, um Statusänderungen reaktiv zu gestalten. Anpassungen am PlanEditorController, um `dayId`-Parameter zu berücksichtigen.

--- src/main/java/de/momogym/controller/WorkoutSessionController.java
+++ src/main/java/de/momogym/controller/WorkoutSessionController.java
@@ -1,5 +1,6 @@
 package de.momogym.controller;
 
+import de.momogym.persistence.PlannedExercise;
 import de.momogym.persistence.TrainingDay;
 import de.momogym.services.TrainingPlanService;
 import jakarta.annotation.PostConstruct;
@@ -9,6 +10,8 @@ import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Named("workoutSessionController")
@@ -21,6 +24,8 @@ public class WorkoutSessionController implements Serializable {
 	private TrainingDay trainingDay;
 	private Long planId; // Um beim "Abbrechen" zurück navigieren zu können
 
+	private List<PlannedExercise> pendingExercises;
+
 	@PostConstruct
 	public void init(){
 		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
@@ -29,10 +34,20 @@ public class WorkoutSessionController implements Serializable {
 
 		if(dayIdParam != null) {
 			this.trainingDay = trainingPlanService.findTrainingDayWithExercises(Long.valueOf(dayIdParam));
+			this.pendingExercises = new ArrayList<>(this.trainingDay.getPlannedExercises());
 		}
 		if(planIdParam != null) {
 			this.planId = Long.valueOf(planIdParam);
 		}
+	}
+
+	public void markAsDone(PlannedExercise exercise) {
+		pendingExercises.remove(exercise);
+		// Später: Hier rufen wir den Service auf, um das Log in der DB zu speichern!
+	}
+
+	public String finishWorkout(){
+		return "planEditor?faces-redirect=true&planId=" + this.planId + "&dayId=" + this.trainingDay.getId();
 	}
 
 	public TrainingDay getTrainingDay() {
@@ -41,5 +56,9 @@ public class WorkoutSessionController implements Serializable {
 
 	public Long getPlanId() {
 		return planId;
+	}
+
+	public List<PlannedExercise> getPendingExercises() {
+		return pendingExercises;
 	}
 }
